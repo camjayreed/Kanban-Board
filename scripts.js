2ffb38dc0c064eee5f527df1e58a5d901ead6664
camjayreed <xtswolf2003@gmail.com>
2026-01-27 06:45:50 -0600

    2 hour mark

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// on dragging an object
+function dragstartHandler(ev) {
+    // figure out what the object is so we know what to do when we drop it
+
+}
+
+// on dragging over an object
+function dragoverHandler(ev) {
+    // maybe highlight the field were dropping into
+
+}
+
+// on dropping an object
+function dropHandler(ev) {
+    // find what the object dropped was and do said thing accordingly
+
+}
